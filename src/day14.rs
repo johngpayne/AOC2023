f6a9735e17ec9e3e6c65189d319b5599f3b53a6f
John Payne <20407779+johngpayne@users.noreply.github.com>
2023-12-14 23:32:09 +0000

    much quicker and easier

--- src/day14.rs
+++ src/day14.rs
@@ -1,11 +1,11 @@
 use glam::{ivec2, IVec2};
 use itertools::Itertools;
-use rustc_hash::{FxHashMap, FxHasher};
-use std::{cmp::Ordering, hash::Hasher};
+use rustc_hash::{FxHashMap, FxHashSet, FxHasher};
+use std::hash::Hasher;
 
 #[tracing::instrument(skip(input), fields(day = 14))]
 pub fn solve(input: &str) -> String {
-    let mut rocks = vec![];
+    let mut rocks = FxHashSet::default();
     let mut rolls = vec![];
     let mut size = IVec2::ZERO;
     input.lines().enumerate().for_each(|(y, line)| {
@@ -14,54 +14,99 @@ pub fn solve(input: &str) -> String {
             size.x = size.x.max(x as i32 + 1);
             let v = ivec2(x as i32, y as i32);
             if ch == '#' {
-                rocks.push(v);
+                rocks.insert(v);
             } else if ch == 'O' {
                 rolls.push(v);
             }
         })
     });
 
-    format!(
-        "{}/{}",
-        part_a(&size, &rocks, &rolls),
-        part_b(&size, &rocks, &rolls)
-    )
-}
+    let mut grids: [Vec<IVec2>; 4] = (0..4)
+        .map(|_| vec![IVec2::ZERO; (size.x * size.y) as usize])
+        .collect_vec()
+        .try_into()
+        .unwrap();
+    for x in 0..size.x {
+        let mut write = ivec2(x, 0);
+        for y in 0..size.y {
+            if rocks.contains(&ivec2(x, y)) {
+                write = ivec2(x, y + 1);
+            }
+            grids[0][(y * size.x + x) as usize] = write;
+        }
+        let mut write = ivec2(x, size.y - 1);
+        for y in (0..size.y).rev() {
+            if rocks.contains(&ivec2(x, y)) {
+                write = ivec2(x, y - 1);
+            }
+            grids[2][(y * size.x + x) as usize] = write;
+        }
+    }
+    for y in 0..size.y {
+        let mut write = ivec2(0, y);
+        for x in 0..size.x {
+            if rocks.contains(&ivec2(x, y)) {
+                write = ivec2(x + 1, y);
+            }
+            grids[1][(y * size.x + x) as usize] = write;
+        }
+        let mut write = ivec2(size.x - 1, y);
+        for x in (0..size.x).rev() {
+            if rocks.contains(&ivec2(x, y)) {
+                write = ivec2(x - 1, y);
+            }
+            grids[3][(y * size.x + x) as usize] = write;
+        }
+    }
 
-fn part_a(size: &IVec2, rocks: &[IVec2], rolls: &[IVec2]) -> i32 {
-    let mut rocks = rocks.to_vec();
-    sort(&mut rocks, 1, -1);
-    let mut rolls = rolls.to_vec();
-    shake(size, &rocks, &mut rolls, 1, -1);
-    score(size, &rolls)
-}
+    let part_a = {
+        let mut rolls = rolls.clone();
+        shake(&size, &mut rolls, &grids[0], IVec2::Y);
+        score(&size, &rolls)
+    };
 
-fn part_b(size: &IVec2, rocks: &[IVec2], rolls: &[IVec2]) -> i32 {
-    let per_dir = (0..4)
-        .map(|dir_index| {
-            let mut rocks = rocks.to_vec();
-            let dom_axis = 1 - (dir_index % 2);
-            let dir = if dir_index < 2 { -1 } else { 1 };
-            sort(&mut rocks, dom_axis, dir);
-            (rocks, dom_axis, dir)
-        })
-        .collect_vec();
-
-    let mut rolls = rolls.to_vec();
-    let mut hashes = FxHashMap::<u64, usize>::default();
-    let mut scores = vec![];
-    loop {
-        per_dir.iter().for_each(|(rocks, dom_axis, dir)| {
-            shake(size, rocks, &mut rolls, *dom_axis, *dir);
-        });
-        let hash = hash(&rolls);
-        if let Some(start_loop) = hashes.get(&hash) {
-            return scores
-                [start_loop + ((1_000_000_000 - 1) - start_loop) % (scores.len() - start_loop)];
+    let part_b = {
+        let mut rolls = rolls.clone();
+        let mut hashes = FxHashMap::<u64, usize>::default();
+        let mut scores = vec![];
+        let mut part_b = None;
+        let mut storage = vec![0u128; size.y as usize];
+        while part_b.is_none() {
+            for (index, grid) in grids.iter().enumerate() {
+                shake(
+                    &size,
+                    &mut rolls,
+                    grid,
+                    [IVec2::Y, IVec2::X, IVec2::NEG_Y, IVec2::NEG_X][index],
+                );
+            }
+            let hash = hash(&mut storage, &rolls);
+            if let Some(start_loop) = hashes.get(&hash) {
+                part_b = Some(
+                    scores[start_loop
+                        + ((1_000_000_000 - 1) - start_loop) % (scores.len() - start_loop)],
+                );
+                break;
+            }
+            hashes.insert(hash, scores.len());
+            scores.push(score(&size, &rolls));
         }
 
-        hashes.insert(hash, scores.len());
-        scores.push(score(size, &rolls));
+        part_b.unwrap()
+    };
+
+    format!("{}/{}", part_a, part_b)
+}
+
+fn shake(size: &IVec2, rolls: &mut [IVec2], grid: &[IVec2], dir: IVec2) {
+    let mut used = vec![0i32; (size.x * size.y).try_into().unwrap()];
+    for roll in rolls.iter_mut() {
+        let nearest_rock = grid[(roll.y * size.x + roll.x) as usize];
+        let used_index = (nearest_rock.y * size.x + nearest_rock.x) as usize;
+        let used_entry = used.get_mut(used_index).unwrap();
+        let new_roll = nearest_rock + dir * *used_entry;
+        *used_entry += 1;
+        *roll = new_roll;
     }
 }
 
@@ -69,60 +114,16 @@ fn score(size: &IVec2, rolls: &[IVec2]) -> i32 {
     rolls.iter().map(|roll| size.y - roll.y).sum::<i32>()
 }
 
-fn hash(rolls: &[IVec2]) -> u64 {
-    let mut hasher = FxHasher::default();
-    rolls.iter().for_each(|roll| {
-        hasher.write_u8(roll.x as u8);
-        hasher.write_u8(roll.y as u8);
-    });
-    hasher.finish()
-}
-
-fn shake(size: &IVec2, rocks: &[IVec2], rolls: &mut [IVec2], dom_axis: usize, dir: i32) {
-    sort(rolls, dom_axis, dir);
-    let reset_pos = if dir < 0 { 0 } else { size[dom_axis] - 1 };
-    let mut rock_index = 0;
-    let mut roll_index = 0;
-    let mut pos = ivec2(-1, -1);
-    while roll_index < rolls.len() {
-        let next_rock = rocks.get(rock_index);
-        let roll = rolls.get_mut(roll_index).unwrap();
-        if roll[1 - dom_axis] == pos[1 - dom_axis]
-            && match next_rock {
-                None => true,
-                Some(next_rock) => {
-                    next_rock[1 - dom_axis] > pos[1 - dom_axis]
-                        || dir * next_rock[dom_axis] < dir * roll[dom_axis]
-                }
-            }
-        {
-            *roll = pos;
-            pos[dom_axis] -= dir;
-            roll_index += 1;
-        } else if match next_rock {
-            None => false,
-            Some(next_rock) => next_rock[1 - dom_axis] == pos[1 - dom_axis],
-        } {
-            pos[dom_axis] = next_rock.unwrap()[dom_axis] - dir;
-            rock_index += 1;
-        } else {
-            pos[1 - dom_axis] += 1;
-            pos[dom_axis] = reset_pos;
-        }
+fn hash(storage_for_rows: &mut [u128], rolls: &[IVec2]) -> u64 {
+    for roll in rolls {
+        storage_for_rows[roll.y as usize] |= 1 << (roll.x as usize);
     }
-}
-
-fn sort(data: &mut [IVec2], dom_axis: usize, dir: i32) {
-    data.sort_by(|i1, i2| {
-        let cmp_non_dom_axis = i1[1 - dom_axis].partial_cmp(&i2[1 - dom_axis]).unwrap();
-        if cmp_non_dom_axis != Ordering::Equal {
-            cmp_non_dom_axis
-        } else {
-            (i2[dom_axis] * dir)
-                .partial_cmp(&(i1[dom_axis] * dir))
-                .unwrap()
-        }
-    });
+    let mut hasher = FxHasher::default();
+    for row in storage_for_rows.iter_mut() {
+        hasher.write_u128(*row);
+        *row = 0;
+    }
+    hasher.finish()
 }
 
 #[tracing::instrument]
