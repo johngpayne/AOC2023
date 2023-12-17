6711320d496379b79f3e50b8db67adefa273184b
John Payne <20407779+johngpayne@users.noreply.github.com>
2023-12-17 15:17:24 +0000

    optimised it

--- src/day17.rs
+++ src/day17.rs
@@ -2,7 +2,6 @@ use std::ops::RangeInclusive;
 
 use glam::{ivec2, IVec2};
 use itertools::Itertools;
-use rustc_hash::FxHashMap;
 
 #[tracing::instrument(skip(input), fields(day = 17))]
 pub fn solve(input: &str) -> String {
@@ -21,101 +20,108 @@ pub fn solve(input: &str) -> String {
 }
 
 fn run(map: &Map, target: IVec2, range: RangeInclusive<u32>) -> u32 {
-    let mut routes = Routes::default();
-    while routes.expand_routes(map, target, range.clone()) {
+    let mut routes = Routes::new(map);
+    while routes.expand_routes(map, target, &range) {}
+    routes.best_costs
+            .iter()
+            .map(|best_costs| best_costs[target.y as usize][target.x as usize])
+            .min()
+            .unwrap()
+}
+
+#[derive(Debug, Clone)]
+struct Route {
+    pos: IVec2,
+    cost: u32,
+    index: usize,
+}
+
+impl Default for Route {
+    fn default() -> Self {
+        Route {
+            pos: IVec2::ZERO,
+            cost: 0,
+            index: usize::MAX,
+        }
     }
-    routes.find_best(target)
 }
 
 #[derive(Debug)]
 struct Routes {
     routes: Vec<Route>,
-    best_costs: FxHashMap<(IVec2, IVec2, u32), u32>,
+    best_costs: [Vec<Vec<u32>>; 4],
 }
 
-impl Default for Routes {
-    fn default() -> Self {
-        Routes {
-            routes: vec![Route::default()],
-            best_costs: [((IVec2::ZERO, IVec2::ZERO, 0), 0)].into_iter().collect(),
-        }
-    }
-}
+const DIRS: [IVec2; 4] = [ivec2(1, 0), ivec2(0, 1), ivec2(-1, 0), ivec2(0, -1)];
 
 impl Routes {
-    fn has_better_route(&self, cost: u32, pos: IVec2, prev_dir: IVec2, dist: u32, min_dist: u32) -> bool {
-        if let Some(&best_cost) = self.best_costs.get(&(pos, prev_dir, dist)) {
-            if best_cost <= cost {
-                return true;
-            }
-        }
-        if dist == min_dist {
-            false
-        } else {
-            self.has_better_route(cost, pos, prev_dir, dist - 1, min_dist)
+    fn new(map: &Map) -> Self {
+        Routes {
+            routes: [Route::default()].into_iter().collect(),
+            best_costs: (0..4)
+                .map(|_| vec![vec![u32::MAX; map.0[0].len()]; map.0.len()])
+                .collect_vec()
+                .try_into()
+                .unwrap(),
         }
     }
-    fn add(&mut self, route: &Route, dist: u32, min_dist: u32) {
-        if self.has_better_route(route.cost, route.pos, route.prev_dir, dist, min_dist) {
-            return;
+    fn add_route(&mut self, route: &Route) {
+        let best_cost =
+            &mut self.best_costs[route.index][route.pos.y as usize][route.pos.x as usize];
+        if *best_cost > route.cost {
+            *best_cost = route.cost;
+            self.routes.push(route.clone());
         }
-        self.best_costs
-            .insert((route.pos, route.prev_dir, dist), route.cost);
-        self.routes.push(route.clone());
     }
-    fn find_best(&self, target: IVec2) -> u32 {
-        self.best_costs
-            .iter()
-            .filter(|((pos, _, _), _)| *pos == target)
-            .map(|(_, cost)| *cost)
-            .min()
-            .unwrap()
-    }
-    fn expand_routes(&mut self, map: &Map, target: IVec2, range: RangeInclusive<u32>) -> bool {
+    fn expand_routes(&mut self, map: &Map, target: IVec2, range: &RangeInclusive<u32>) -> bool {
         let mut prev_routes = Vec::default();
         std::mem::swap(&mut prev_routes, &mut self.routes);
         for route in prev_routes {
-            if route.pos != target {
-                const DIRS: [IVec2; 4] = [ivec2(1, 0), ivec2(0, 1), ivec2(-1, 0), ivec2(0, -1)];
-                for dir in DIRS {
-                    if route.prev_dir != -dir && route.prev_dir != dir {
-                        let mut expanded_route = route.clone();
-                        for dist in 1..=*range.end() {
-                            if let Some(cost) = map.get(expanded_route.pos + dir) {
-                                expanded_route.pos += dir;
-                                expanded_route.cost += cost;
-                                expanded_route.prev_dir = dir;
-                                if dist >= *range.start() {
-                                    self.add(&expanded_route, dist, *range.start());
-                                }
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-                }
+            if route.index == usize::MAX {
+                (0..4).for_each(|index| {
+                    self.move_along_route(&route, index, map, range);
+                });
+            } else if route.pos != target {
+                [
+                    (route.index + 1) % 4,
+                    (route.index + 3) % 4,
+                ]
+                .into_iter()
+                .for_each(|index| {
+                    self.move_along_route(&route, index, map, range);
+                });
             }
         }
         !self.routes.is_empty()
+    }
+    fn move_along_route(&mut self, route: &Route, index: usize, map: &Map, range: &RangeInclusive<u32>) {
+        let mut expanded_route = Route {
+            index,
+            ..route.clone()
+        };
+        for dist in 1..=*range.end() {
+            if let Some(cost) = map.get(expanded_route.pos + DIRS[index]) {
+                expanded_route.pos += DIRS[index];
+                expanded_route.cost += cost;
+                if dist >= *range.start() {
+                    self.add_route(&expanded_route);
+                }
+            } else {
+                return;
+            }
+        }
     }
 }
 
 struct Map(Vec<Vec<u32>>);
 
 impl Map {
-    fn get(&self, pos: IVec2) -> Option<u32> {
+    fn get(&self, pos: IVec2) -> Option<&u32> {
         self.0
             .get(pos.y as usize)
-            .map(|row| row.get(pos.x as usize).copied())
+            .map(|row| row.get(pos.x as usize))
             .unwrap_or_default()
     }
-}
-
-#[derive(Debug, Default, Clone)]
-struct Route {
-    pos: IVec2,
-    cost: u32,
-    prev_dir: IVec2,
 }
 
 #[tracing::instrument]
