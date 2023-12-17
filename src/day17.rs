4c04951910ddf4f65c53d45f8cf65c453c7b2fc4
John Payne <20407779+johngpayne@users.noreply.github.com>
2023-12-17 18:38:59 +0000

    lot simpler and found i wasn't testing routes were the current best one at that point when expanding

--- src/day17.rs
+++ src/day17.rs
@@ -1,7 +1,6 @@
-use std::ops::RangeInclusive;
-
 use glam::{ivec2, IVec2};
 use itertools::Itertools;
+use std::collections::VecDeque;
 
 #[tracing::instrument(skip(input), fields(day = 17))]
 pub fn solve(input: &str) -> String {
@@ -16,111 +15,87 @@ pub fn solve(input: &str) -> String {
         .collect_vec());
     let target = ivec2(map.0[0].len() as i32 - 1, map.0.len() as i32 - 1);
 
-    format!("{}/{}", run(&map, target, 1..=3), run(&map, target, 4..=10))
+    format!(
+        "{}/{}",
+        expand_routes::<1, 3>(&map, target),
+        expand_routes::<4, 10>(&map, target)
+    )
 }
 
-fn run(map: &Map, target: IVec2, range: RangeInclusive<u32>) -> u32 {
-    let mut routes = Routes::new(map);
-    while routes.expand_routes(map, target, &range) {}
-    routes.best_costs
-            .iter()
-            .map(|best_costs| best_costs[target.y as usize][target.x as usize])
-            .min()
-            .unwrap()
+fn expand_routes<const MIN: u32, const MAX: u32>(map: &Map, target: IVec2) -> u32 {
+    let mut routes = [Route {
+        pos: IVec2::ZERO,
+        cost: 0,
+        index: usize::MAX,
+    }]
+    .into_iter()
+    .collect::<VecDeque<_>>();
+    let mut best_costs = [0, 1].map(|_| vec![vec![u32::MAX; map.0[0].len()]; map.0.len()]);
+
+    while !routes.is_empty() {
+        let route = routes.pop_front().unwrap();
+        if route.pos != target {
+            let best_cost = if route.index != usize::MAX {
+                best_costs[route.index & 1][route.pos.y as usize][route.pos.x as usize]
+            } else {
+                0
+            };
+            if route.cost == best_cost {
+                if route.index == usize::MAX {
+                    [0, 1]
+                } else {
+                    [(route.index + 1) % 4, (route.index + 3) % 4]
+                }
+                .into_iter()
+                .for_each(|index| {
+                    let mut route = route;
+                    route.index = index;
+                    for dist in 1..=MAX {
+                        route.pos += DIRS[index];
+                        if let Some(cost) = map.get(route.pos, target) {
+                            route.cost += cost;
+                            if dist >= MIN {
+                                let best_cost = &mut best_costs[route.index & 1]
+                                    [route.pos.y as usize]
+                                    [route.pos.x as usize];
+                                if route.cost < *best_cost {
+                                    *best_cost = route.cost;
+                                    routes.push_back(route);
+                                }
+                            }
+                        } else {
+                            break;
+                        }
+                    }
+                });
+            }
+        }
+    }
+    best_costs
+        .iter()
+        .map(|best_costs| best_costs[target.y as usize][target.x as usize])
+        .min()
+        .unwrap()
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone)]
 struct Route {
     pos: IVec2,
     cost: u32,
     index: usize,
 }
 
-impl Default for Route {
-    fn default() -> Self {
-        Route {
-            pos: IVec2::ZERO,
-            cost: 0,
-            index: usize::MAX,
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Routes {
-    routes: Vec<Route>,
-    best_costs: [Vec<Vec<u32>>; 4],
-}
-
 const DIRS: [IVec2; 4] = [ivec2(1, 0), ivec2(0, 1), ivec2(-1, 0), ivec2(0, -1)];
-
-impl Routes {
-    fn new(map: &Map) -> Self {
-        Routes {
-            routes: [Route::default()].into_iter().collect(),
-            best_costs: (0..4)
-                .map(|_| vec![vec![u32::MAX; map.0[0].len()]; map.0.len()])
-                .collect_vec()
-                .try_into()
-                .unwrap(),
-        }
-    }
-    fn add_route(&mut self, route: &Route) {
-        let best_cost =
-            &mut self.best_costs[route.index][route.pos.y as usize][route.pos.x as usize];
-        if *best_cost > route.cost {
-            *best_cost = route.cost;
-            self.routes.push(route.clone());
-        }
-    }
-    fn expand_routes(&mut self, map: &Map, target: IVec2, range: &RangeInclusive<u32>) -> bool {
-        let mut prev_routes = Vec::default();
-        std::mem::swap(&mut prev_routes, &mut self.routes);
-        for route in prev_routes {
-            if route.index == usize::MAX {
-                (0..4).for_each(|index| {
-                    self.move_along_route(&route, index, map, range);
-                });
-            } else if route.pos != target {
-                [
-                    (route.index + 1) % 4,
-                    (route.index + 3) % 4,
-                ]
-                .into_iter()
-                .for_each(|index| {
-                    self.move_along_route(&route, index, map, range);
-                });
-            }
-        }
-        !self.routes.is_empty()
-    }
-    fn move_along_route(&mut self, route: &Route, index: usize, map: &Map, range: &RangeInclusive<u32>) {
-        let mut expanded_route = Route {
-            index,
-            ..route.clone()
-        };
-        for dist in 1..=*range.end() {
-            if let Some(cost) = map.get(expanded_route.pos + DIRS[index]) {
-                expanded_route.pos += DIRS[index];
-                expanded_route.cost += cost;
-                if dist >= *range.start() {
-                    self.add_route(&expanded_route);
-                }
-            } else {
-                return;
-            }
-        }
-    }
-}
 
 struct Map(Vec<Vec<u32>>);
 
 impl Map {
-    fn get(&self, pos: IVec2) -> Option<&u32> {
-        self.0
-            .get(pos.y as usize)
-            .map(|row| row.get(pos.x as usize))
-            .unwrap_or_default()
+    fn get(&self, pos: IVec2, target: IVec2) -> Option<u32> {
+        if pos.x >= 0 && pos.y >= 0 && pos.x <= target.x && pos.y <= target.y {
+            Some(self.0[pos.y as usize][pos.x as usize])
+        } else {
+            None
+        }
     }
 }
 
