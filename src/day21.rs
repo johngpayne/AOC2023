0cb9bbd668f27b9c0bae14407baea58c4d23e091
John Payne <20407779+johngpayne@users.noreply.github.com>
2023-12-22 16:09:51 +0000

    tidy up

--- src/day21.rs
+++ src/day21.rs
@@ -41,7 +41,7 @@ fn parse(input: &str) -> (Vec<Vec<char>>, IVec2) {
     (map_chars, start_pos)
 }
 
-type Starts = VecDeque<(u64, usize, IVec2)>;
+type PageRef = (u64, usize, IVec2);
 type Cache = FxHashMap<u64, Page>;
 type CacheItem = <Cache as IntoIterator>::Item;
 
@@ -81,71 +81,71 @@ impl Map {
     }
 
     fn part_b(&self, steps: usize) -> usize {
-        let mut starts: Starts = [(0, steps, IVec2::ZERO)]
+        let mut active_page_refs: VecDeque<PageRef> = [(0, steps, IVec2::ZERO)]
             .into_iter()
             .collect::<VecDeque<_>>();
         let mut cached_pages: Cache = [(0, Page::new(self, &[(0, self.start_pos)], 0, steps & 1))]
             .into_iter()
             .collect::<FxHashMap<_, _>>();
-        let mut current_page: Option<&Page> = None;
+        let mut page: Option<&Page> = None;
         let mut total_score = 0;
 
-        while !starts.is_empty() {
-            let (page_hash, page_steps, page_pos) = starts.pop_front().unwrap();
+        while !active_page_refs.is_empty() {
+            let (page_hash, steps_left, page_pos) = active_page_refs.pop_front().unwrap();
 
-            if current_page.is_none() || current_page.unwrap().hash != page_hash {
-                current_page = cached_pages.get(&page_hash);
+            if page.is_none() || page.unwrap().hash != page_hash {
+                page = cached_pages.get(&page_hash);
             }
-            total_score += current_page.unwrap().score(page_steps, steps & 1);
+            total_score += page.unwrap().score(steps_left, steps & 1);
 
             let mut new_pages: Vec<CacheItem> = vec![];
 
-            let mut expand = |pos: IVec2, dir, steps_left: usize| {
-                let new_pos = pos + dir;
-                let (min_in_dir, border_hash, border) = current_page.unwrap().get_border(dir);
+            let mut expand = |pos: IVec2, dir| {
+                let pos_in_dir = pos + dir;
+                let (steps_to_border, border_hash, border_starts) = page.unwrap().get_border(dir);
 
-                if steps_left >= *min_in_dir {
+                if steps_left >= *steps_to_border {
                     if *border_hash == page_hash {
-                        if pos.x != 0 {
+                        if dir.x != 0 {
                             // short cut for going along using same hash each time...
                             let mut steps_left = steps_left;
                             loop {
-                                steps_left -= *min_in_dir;
-                                total_score += current_page.unwrap().score(steps_left, steps & 1);
-                                if steps_left < *min_in_dir {
+                                steps_left -= *steps_to_border;
+                                total_score += page.unwrap().score(steps_left, steps & 1);
+                                if steps_left < *steps_to_border {
                                     break;
                                 }
                             }
                         } else {
                             // push to front so same page next time
-                            starts.push_front((*border_hash, steps_left - min_in_dir, new_pos));
+                            active_page_refs.push_front((*border_hash, steps_left - steps_to_border, pos_in_dir));
                         }
                     } else {
                         if !cached_pages.contains_key(border_hash) {
                             new_pages.push((
                                 *border_hash,
-                                Page::new(self, border, *border_hash, steps & 1),
+                                Page::new(self, border_starts, *border_hash, steps & 1),
                             ));
                         }
-                        starts.push_back((*border_hash, steps_left - min_in_dir, new_pos));
+                        active_page_refs.push_back((*border_hash, steps_left - steps_to_border, pos_in_dir));
                     }
                 }
             };
 
             if page_pos == IVec2::ZERO {
                 for &dir in DIRS.iter() {
-                    expand(page_pos, dir, page_steps);
+                    expand(page_pos, dir);
                 }
             } else if page_pos.x == 0 {
-                expand(page_pos, page_pos.signum(), page_steps);
-                expand(page_pos, IVec2::X, page_steps);
-                expand(page_pos, -IVec2::X, page_steps);
+                expand(page_pos, page_pos.signum());
+                expand(page_pos, IVec2::X);
+                expand(page_pos, -IVec2::X);
             } else {
-                expand(page_pos, ivec2(page_pos.x.signum(), 0), page_steps);
+                expand(page_pos, ivec2(page_pos.x.signum(), 0));
             }
 
             if !new_pages.is_empty() {
-                current_page = None;
+                page = None;
                 cached_pages.extend(new_pages.into_iter());
             }
         }
@@ -159,8 +159,8 @@ type Border = (usize, u64, Vec<StepAndPos>);
 
 struct Page {
     min_steps_to_pos: Vec<Option<usize>>,
-    max_steps: usize,
-    score_at_max_steps: usize,
+    min_steps_to_fill: usize,
+    score_when_full: usize,
     borders: [Border; 4],
     hash: u64,
 }
@@ -172,14 +172,16 @@ impl Page {
         &self.borders[DIRS.iter().position(|&test_dir| test_dir == dir).unwrap()]
     }
     fn score(&self, steps: usize, odd: usize) -> usize {
-        if steps >= self.max_steps {
-            self.score_at_max_steps
+        if steps >= self.min_steps_to_fill {
+            self.score_when_full
         } else {
             self.min_steps_to_pos
                 .iter()
                 .filter(|&&min_steps| match min_steps {
                     None => false,
-                    Some(min_steps) => steps >= min_steps && (min_steps % 2) == odd,
+                    Some(min_steps_to_pos) => {
+                        steps >= min_steps_to_pos && (min_steps_to_pos % 2) == odd
+                    }
                 })
                 .count()
         }
@@ -204,16 +206,6 @@ impl Page {
 
         let get_indexed = |min_steps_to_pos: &Vec<Option<usize>>, index: i32, dir: IVec2| {
             get(min_steps_to_pos, get_indexed_coord(index, dir))
-        };
-
-        let hash_border = |border: &[StepAndPos], dir: IVec2| {
-            let mut hasher = FxHasher::default();
-            hasher.write_i32(dir.x);
-            hasher.write_i32(dir.y);
-            for (steps, _) in border.iter() {
-                hasher.write_usize(*steps);
-            }
-            hasher.finish()
         };
 
         let mut next_wave = vec![];
@@ -243,13 +235,13 @@ impl Page {
             }
         }
 
-        let max_steps = min_steps_to_pos
+        let min_steps_to_fill = min_steps_to_pos
             .iter()
             .filter_map(|steps| *steps)
             .max()
             .unwrap();
 
-        let score_at_max_steps = min_steps_to_pos
+        let score_when_full = min_steps_to_pos
             .iter()
             .filter(|&&min_steps| match min_steps {
                 None => false,
@@ -258,24 +250,37 @@ impl Page {
             .count();
 
         let borders = DIRS.map(|dir| {
-            let min_in_dir = (0..map.size).fold(usize::MAX, |min, index| {
+            
+            let steps_to_border = (0..map.size).fold(usize::MAX, |min, index| {
                 min.min(get_indexed(&min_steps_to_pos, index, dir).unwrap())
             }) & !1;
+            
             let border = (0..map.size)
                 .map(|index| {
                     (
-                        1 + get_indexed(&min_steps_to_pos, index, dir).unwrap() - min_in_dir,
+                        1 + get_indexed(&min_steps_to_pos, index, dir).unwrap() - steps_to_border,
                         get_indexed_coord(index, -dir),
                     )
                 })
                 .collect_vec();
-            (min_in_dir, hash_border(&border, dir), border)
+
+            let border_hash = {
+                let mut hasher = FxHasher::default();
+                hasher.write_i32(dir.x);
+                hasher.write_i32(dir.y);
+                for (steps, _) in border.iter() {
+                    hasher.write_usize(*steps);
+                }
+                hasher.finish()
+            };
+
+            (steps_to_border, border_hash, border)
         });
 
         Page {
             min_steps_to_pos,
-            max_steps,
-            score_at_max_steps,
+            min_steps_to_fill,
+            score_when_full,
             borders,
             hash,
         }
