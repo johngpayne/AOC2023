26e5c594bf8a1b38474c123b66d921db8be7f587
John Payne <20407779+johngpayne@users.noreply.github.com>
2023-12-23 17:24:45 +0000

    tidy

--- src/day23.rs
+++ src/day23.rs
@@ -6,6 +6,13 @@ use rustc_hash::FxHashMap;
 
 const DIRS: [IVec2; 4] = [ivec2(1, 0), ivec2(0, 1), ivec2(-1, 0), ivec2(0, -1)];
 
+type NodeEntry = (IVec2, usize);
+type Node = [Option<NodeEntry>; 4];
+
+fn inverse_dir(dir_index: u8) -> u8 {
+    (dir_index + 2) % 4
+}
+
 struct Map {
     grid: Vec<u8>,
     size: IVec2,
@@ -81,7 +88,6 @@ impl Map {
     }
 
     fn longest_route(&self) -> usize {
-        
         // get pos -> [maybe (to_pos, len)]
         let mut nodes = FxHashMap::<IVec2, Node>::default();
         let mut new_positions: Vec<IVec2> = vec![self.start];
@@ -102,11 +108,16 @@ impl Map {
                             loop {
                                 pos += DIRS[current_index as usize];
                                 len += 1;
-                                if pos == self.end || pos == self.start || self.get(pos).count_ones() > 2 {
+                                if pos == self.end
+                                    || pos == self.start
+                                    || self.get(pos).count_ones() > 2
+                                {
                                     ret = Some((pos, len));
                                     break;
                                 }
-                                if self.get(pos).count_ones() == 1 && !self.can_move(pos, current_index) {
+                                if self.get(pos).count_ones() == 1
+                                    && !self.can_move(pos, current_index)
+                                {
                                     break;
                                 }
                                 current_index = (0..4)
@@ -127,16 +138,23 @@ impl Map {
             }
         }
         assert!(nodes.len() < 64);
-        
+
         // get pos -> index in nodes map
-        let pos_to_index = nodes.iter().enumerate().map(|(index, (pos, _))| {
-            (*pos, index as u8)
-        }).collect::<FxHashMap<_, _>>();
-        
+        let pos_to_index = nodes
+            .iter()
+            .enumerate()
+            .map(|(index, (pos, _))| (*pos, index as u8))
+            .collect::<FxHashMap<_, _>>();
+
         // get [[maybe (index, len)>]]
-        let index_nodes = nodes.values().map(|node_entries| {
-            node_entries.map(|entry| entry.map(|(to_pos, len)| (pos_to_index.get(&to_pos).unwrap(), len)))
-        }).collect_vec();
+        let index_nodes = nodes
+            .values()
+            .map(|node_entries| {
+                node_entries.map(|entry| {
+                    entry.map(|(to_pos, len)| (pos_to_index.get(&to_pos).unwrap(), len))
+                })
+            })
+            .collect_vec();
 
         let end_index = *pos_to_index.get(&self.end).unwrap();
         let mut wave = VecDeque::from([(*pos_to_index.get(&self.start).unwrap(), 0usize, 0u64)]);
@@ -146,24 +164,17 @@ impl Map {
 
             if index == end_index {
                 results.push(len);
-                continue;
-            }
-            visited |= 1 << index;
-            for &(to_index, to_len) in index_nodes[index as usize].iter().flatten() {
-                if (visited & 1 << to_index) == 0 {
-                    wave.push_back((*to_index, len + to_len, visited));
+            } else {
+                visited |= 1 << index;
+                for &(to_index, to_len) in index_nodes[index as usize].iter().flatten() {
+                    if (visited & 1 << to_index) == 0 {
+                        wave.push_back((*to_index, len + to_len, visited));
+                    }
                 }
             }
         }
         results.into_iter().max().unwrap()
     }
-}
-
-type NodeEntry = (IVec2, usize);
-type Node = [Option<NodeEntry>; 4];
-
-fn inverse_dir(dir_index: u8) -> u8 {
-    (dir_index + 2) % 4
 }
 
 #[tracing::instrument(skip(input), fields(day = 23))]
@@ -172,7 +183,11 @@ pub fn solve(input: &str) -> String {
         .lines()
         .map(|line| line.trim().chars().collect_vec())
         .collect_vec();
-    format!("{}/{}", Map::new(&grid_ch, false).longest_route(), Map::new(&grid_ch, true).longest_route())
+    format!(
+        "{}/{}",
+        Map::new(&grid_ch, false).longest_route(),
+        Map::new(&grid_ch, true).longest_route()
+    )
 }
 
 #[tracing::instrument]
